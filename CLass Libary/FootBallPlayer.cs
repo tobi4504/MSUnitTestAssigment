cafa5f11fac9ea1994fdf8c5ae0caaa928fe9599
DESKTOP-PV67OOQ\tobia <tobi4504@edu.easj.dk>
2021-09-14 13:47:18 +0200

    Initial commit

---
+++ CLass Libary/FootBallPlayer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace CLass_Libary
+{
+    /// <summary>
+    ///  This class Function is to create instances of football players 
+    /// </summary>
+    public class FootBallPlayer
+    {
+        /// <summary>
+        ///  This constructor is an open constructor where you can create an object with null values on all properties
+        ///  
+        /// </summary>
+        public FootBallPlayer()
+        {
+
+        }
+        public FootBallPlayer(int id,string name, double price, int shirtName)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            ShirtNumber = shirtName;
+        }
+        /// <param Id>Id is ust to identify different instances of footplayers </param>
+        public int Id { get; set; }
+
+        /// <param Name> 
+        /// Name has to be atleast 4 chars long, if name is not 4 chars long the system 
+        /// will throw exception telling user that it must be atlesat 4 chars long. 
+        /// </param>
+
+        private string name;
+        public string Name   
+        {
+            get { return name; }  
+            set { if(value.Length >= 4)
+                {
+                    name = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Name must contain atlesat 4 characters");
+                }
+                } 
+        }
+
+        /// <param Price> 
+        // Shows the price of a given footballplayer, the footballplayers' price must be over 0, if not the system will throw an excpetion telling the user that the price must be over 0.
+        /// </param>
+   
+        private double price;
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (value > 0)
+                {
+                    price = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Price must be over 0");
+                }
+            }
+        }
+        /// <param shirtNumber> 
+         // shirtnumber shows which number the specefic player has on he's shirt, and can only be a number between 0 and 101 /
+        /// </param>
+        private int shirtNumber;
+        public int ShirtNumber
+        {
+            get { return shirtNumber; }
+            set
+            {
+                if (value > 0 && value < 101)
+                {
+                    shirtNumber = value;
+                }
+                else if (value > 0)
+                {
+                    throw new ArgumentOutOfRangeException("You must place a value higher then 0 and between 0 and 101");
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("You must place a value lower then 101 and between 0 and 101");
+                }
+            }
+        }
+
+    }
+}
---
+++ FootBallTest/FootBallPlayerTset.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CLass_Libary;
+using System;
+
+namespace FootBallPlayerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private CLass_Libary.FootBallPlayer Player;
+
+        [TestInitialize]
+        public void CreatPlayer()
+        {
+
+            Player = new CLass_Libary.FootBallPlayer(1, "Boris", 2, 3);
+        }
+        [TestMethod]
+        public void TestingConstructorInput()
+        {
+            Assert.AreEqual(1, Player.Id);
+            Assert.AreEqual("Boris", Player.Name);
+            Assert.AreEqual(2, Player.Price);
+            Assert.AreEqual(3, Player.ShirtNumber);
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "Name must contain atlesat 4 characters")]
+        public void TestingConstructorName()
+        {
+            Player = new CLass_Libary.FootBallPlayer(1, "Bor", 2, 3);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "Price must be over 0")]
+        public void TestingConstructorPrice()
+        {
+            Player = new CLass_Libary.FootBallPlayer(1, "Boris", 0, 3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "You must place a value higher then 0 and between 0 and 101")]
+        public void TestingConstructorShirtNrLow()
+        {
+            Player = new CLass_Libary.FootBallPlayer(1, "Boris", 1, 0);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "You must place a value higher then 0 and between 0 and 101")]
+        public void TestingConstructorShirtNrHigh()
+        {
+            Player = new CLass_Libary.FootBallPlayer(1, "Boris", 1, 101);
+        }
+
+
+        [TestMethod]
+        public void TestingConstructorNoInput()
+        {
+            Player = new CLass_Libary.FootBallPlayer();
+            Assert.AreEqual(0, Player.Id);
+            Assert.AreEqual(null, Player.Name);
+            Assert.AreEqual(0, Player.Price);
+            Assert.AreEqual(0, Player.ShirtNumber);
+
+        }
+        [TestMethod]
+        public void AddingNameToPlayer()
+        {
+            Player.Name = "Morten";
+            Assert.AreEqual("Morten", Player.Name);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "Name must contain atlesat 4 characters")]
+        public void AddingNoneValidNameToPlayer()
+        {
+            // Because we know no footballplayer can be named bo
+            Player.Name = "Bo";
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+
+        public void AddingNullNameToPlayer()
+        {
+            Player.Name = null;
+        }
+
+        [TestMethod]
+        public void Adding4CharNameToPlayer()
+        {
+
+            Player.Name = "Bebo";
+            Assert.AreEqual("Bebo", Player.Name);
+
+        }
+        [TestMethod]
+        public void Adding3CharNameToPlayer()
+        {
+            try
+            {
+                Player.Name = "Beb";
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+
+            }
+        }
+        [TestMethod]
+        public void SettingPrice1Player()
+        {
+            Player.Price = 1;
+            Assert.AreEqual(1, Player.Price);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "Price must be over 0")]
+        public void SettingPrice0ToPlayer()
+        {
+            Player.Price = 0;
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "You must place a value higher then 0 and between 0 and 101")]
+        public void SettingShirt0Player()
+        {
+            Player.ShirtNumber = 0;
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "You must place a value higher then 0 and between 0 and 101")]
+        public void SettingShirt101Player()
+        {
+            Player.ShirtNumber = 101;
+        }
+        [TestMethod]
+        public void SettingShirt100Player()
+        {
+            Player.ShirtNumber = 100;
+            Assert.AreEqual(100, Player.ShirtNumber);
+        }
+        [TestMethod]
+        public void SettingShirt1Player()
+        {
+            Player.ShirtNumber = 1;
+            Assert.AreEqual(1, Player.ShirtNumber);
+        }
+
+
+
+    }
+}
+
